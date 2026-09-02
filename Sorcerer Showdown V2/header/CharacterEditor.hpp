9b28b3e3917020cb98c6b3a23f2dccf70a84dcd2
Can Batu Karakoyunlu <batucankarakoyunlu@gmail.com>
2026-09-02 17:51:46 +0300

    made charactereditor functions static

--- Sorcerer Showdown V2/header/CharacterEditor.hpp
+++ Sorcerer Showdown V2/header/CharacterEditor.hpp
@@ -18,28 +18,28 @@ enum class ItemPlacement {
 
 struct CharacterEditor {
     // identity section
-    void SetName(Character& c, std::string_view name);
-    void SetColor(Character& c, std::string_view color);
+    static void SetName(Character& c, std::string_view name);
+    static void SetColor(Character& c, std::string_view color);
     // base state section
-    void SetMaxHealth(Character& c, double hp);
-    void SetInvulnerability(Character& c, bool t);
+    static void SetMaxHealth(Character& c, double hp);
+    static void SetInvulnerability(Character& c, bool t);
     // inventory
-    void GiveCharacterTool(Character& c, std::unique_ptr<int> tool, ItemPlacement place);
-    void SetInventoryAccess(Character& c, bool t);
+    static void GiveCharacterTool(Character& c, std::unique_ptr<int> tool, ItemPlacement place);
+    static void SetInventoryAccess(Character& c, bool t);
     /*                        Character End                       */
     // inside curse user
-    void SetCursedEnergyEfficiency(CurseUser& c, CurseUser::CEfficiency efficiency);
+    static void SetCursedEnergyEfficiency(CurseUser& c, CurseUser::CEfficiency efficiency);
     // curse user system
-    void AddBindingVow(CurseUser& c, std::unique_ptr<int> vow);
-    void AddShikigami(CurseUser& c, std::unique_ptr<int> shk);
-    void SetTechnique(CurseUser& c, std::unique_ptr<int> tech);
-    void SetDomain(CurseUser& c, std::unique_ptr<int> domain);
-    void SetDomainNullifier(CurseUser& c, std::unique_ptr<int> dnull);
+    static void AddBindingVow(CurseUser& c, std::unique_ptr<int> vow);
+    static void AddShikigami(CurseUser& c, std::unique_ptr<int> shk);
+    static void SetTechnique(CurseUser& c, std::unique_ptr<int> tech);
+    static void SetDomain(CurseUser& c, std::unique_ptr<int> domain);
+    static void SetDomainNullifier(CurseUser& c, std::unique_ptr<int> dnull);
     // traits
-    void SetTraitSixEyes(CurseUser& c, bool t);
-    void SetTraitPassiveHealing(CurseUser& c, bool t);
+    static void SetTraitSixEyes(CurseUser& c, bool t);
+    static void SetTraitPassiveHealing(CurseUser& c, bool t);
     /*                        CurseUser End                       */
     // sorcerer system
-    void SetReverseCursedTechnique(Sorcerer& c, bool can_use);
-    void SetReverseCursedTechniqueLevel(Sorcerer& c, Sorcerer::RCTLevel lvl);
+    static void SetReverseCursedTechnique(Sorcerer& c, bool can_use);
+    static void SetReverseCursedTechniqueLevel(Sorcerer& c, Sorcerer::RCTLevel lvl);
 };
