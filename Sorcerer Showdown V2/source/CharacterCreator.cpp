efad62a4198ee71fe82fc16d6583583a1387b784
Can Batu Karakoyunlu <batucankarakoyunlu@gmail.com>
2026-09-03 18:08:12 +0300

    small update to character creator functions

--- Sorcerer Showdown V2/source/CharacterCreator.cpp
+++ Sorcerer Showdown V2/source/CharacterCreator.cpp
@@ -9,46 +9,70 @@
 
 // character
 std::unique_ptr<Character> Create::TranfiguredHuman() {
-    auto tr = std::make_unique<Character>();
-    Editor::SetName(*tr, "Transfigured Human");
-    Editor::SetColor(*tr, "\x1b[91m");
-    Editor::SetHealth(*tr, get_random<double>(1.0, 100.0));
-    Editor::SetDurability(*tr, get_random<double>(1.0, 100.0));
-    Editor::SetStrength(*tr, get_random<double>(1.0, 100.0));
-    return tr;
+    auto c = std::make_unique<Character>();
+
+    constexpr const char* name = "Transfigured Human";
+    constexpr const char* color = "\x1b[91m";
+    const double health = get_random<double>(1.0, 100.0);
+    const double strength = get_random<double>(1.0, 100.0);
+    const double durability = get_random<double>(1.0, 100.0);
+
+    Editor::SetName(*c, name);
+    Editor::SetColor(*c, color);
+    Editor::SetHealth(*c, health);
+    Editor::SetDurability(*c, durability);
+    Editor::SetStrength(*c, strength);
+
+    return c;
 }
 // curse user
 std::unique_ptr<CurseUser> Create::Mahito() {
-    auto mh = std::make_unique<CurseUser>();
-    double health = 550.0, cursed_energy = 4000.0, strength = 115.0;
-    double durability = 75.0;
-    Editor::SetName(*mh, "Mahito");
-    Editor::SetColor(*mh, "\x1b[91m");
-    Editor::SetHealth(*mh, health);
-    Editor::SetDurability(*mh, durability);
-    Editor::SetStrength(*mh, strength);
-    Editor::SetCursedEnergy(*mh, cursed_energy);
-    Editor::SetDomain(*mh, std::unique_ptr<int>()); // placeholder
-    Editor::SetTechnique(*mh, std::unique_ptr<int>()); // placeholder
-    Editor::SetCursedEnergyEfficiency(*mh, CurseUser::CEfficiency::Stable);
-    Editor::SetTraitPassiveHealing(*mh, true);
-    return mh;
+    auto c = std::make_unique<CurseUser>();
+
+    constexpr const char* name = "Mahito";
+    constexpr const char* color = "\x1b[91m";
+    constexpr double health = 550.0;
+    constexpr double cursed_energy = 4000.0;
+    constexpr double strength = 115.0;
+    constexpr double durability = 75.0;
+    constexpr auto ce_efficiency = CurseUser::CEfficiency::Stable;
+
+    Editor::SetName(*c, name);
+    Editor::SetColor(*c, color);
+    Editor::SetHealth(*c, health);
+    Editor::SetDurability(*c, durability);
+    Editor::SetStrength(*c, strength);
+    Editor::SetCursedEnergy(*c, cursed_energy);
+    Editor::SetDomain(*c, std::unique_ptr<int>()); // placeholder
+    Editor::SetTechnique(*c, std::unique_ptr<int>()); // placeholder
+    Editor::SetCursedEnergyEfficiency(*c, ce_efficiency);
+    Editor::SetTraitPassiveHealing(*c, true);
+
+    return c;
 }
 // sorcerer
 std::unique_ptr<Sorcerer> Create::Gojo() {
-    auto gj = std::make_unique<Sorcerer>();
-    double health = 1000.0, cursed_energy = 5000.0, strength = 185.0;
-    double durability = 300.0;
-    Editor::SetName(*gj, "Gojo");
-    Editor::SetColor(*gj, "\x1b[91m");
-    Editor::SetHealth(*gj, health);
-    Editor::SetDurability(*gj, durability);
-    Editor::SetStrength(*gj, strength);
-    Editor::SetCursedEnergy(*gj, cursed_energy);
-    Editor::SetDomain(*gj, std::unique_ptr<int>()); // placeholder
-    Editor::SetDomainNullifier(*gj, std::unique_ptr<int>()); // placeholder
-    Editor::SetTechnique(*gj, std::unique_ptr<int>()); // placeholder
-    Editor::SetCursedEnergyEfficiency(*gj, CurseUser::CEfficiency::Extreme);
-    Editor::SetTraitSixEyes(*gj, true);
-    return gj;
+    auto c = std::make_unique<Sorcerer>();
+
+    constexpr const char* name = "Gojo";
+    constexpr const char* color = "\x1b[91m";
+    constexpr double health = 1000.0;
+    constexpr double cursed_energy = 5000.0;
+    constexpr double strength = 185.0;
+    constexpr double durability = 300.0;
+    constexpr auto ce_efficiency = CurseUser::CEfficiency::Extreme;
+
+    Editor::SetName(*c, name);
+    Editor::SetColor(*c, color);
+    Editor::SetHealth(*c, health);
+    Editor::SetDurability(*c, durability);
+    Editor::SetStrength(*c, strength);
+    Editor::SetCursedEnergy(*c, cursed_energy);
+    Editor::SetDomain(*c, std::unique_ptr<int>()); // placeholder
+    Editor::SetDomainNullifier(*c, std::unique_ptr<int>()); // placeholder
+    Editor::SetTechnique(*c, std::unique_ptr<int>()); // placeholder
+    Editor::SetCursedEnergyEfficiency(*c, ce_efficiency);
+    Editor::SetTraitSixEyes(*c, true);
+
+    return c;
 }
