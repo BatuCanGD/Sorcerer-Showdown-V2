bd674df51653e20731fd5ecc6435c25814acca34
Can Batu Karakoyunlu <batucankarakoyunlu@gmail.com>
2026-09-15 14:14:23 +0300

    removed unused functions

--- Sorcerer Showdown V2/header/Systems/System/DomainSystem.hpp
+++ Sorcerer Showdown V2/header/Systems/System/DomainSystem.hpp
@@ -25,8 +25,5 @@ enum class DomainWinCon : std::uint8_t {
 namespace DomainSystem {
     std::pair<bool, double> CalculateHit(const std::optional<Domain>& domain, const std::unique_ptr<Character>& c); 
     std::pair<ClashWinner, DomainWinCon> ClashDomains(std::optional<Domain>& first, std::optional<Domain>& second); // use case for already active domains
-    
     void ResetDomain(std::optional<Domain>& domain);
-    constexpr const char* DomainWinType(DomainWinCon win_condition);
-    constexpr const char* DomainClashWinner(ClashWinner winner);
 };
