2fedf44dd4f07c917f8bdb9e2d885be0e00db258
Can Batu Karakoyunlu <batucankarakoyunlu@gmail.com>
2026-09-16 17:07:42 +0300

    updated and solidified shikigami

--- Sorcerer Showdown V2/header/Stuff/Shikigami.hpp
+++ Sorcerer Showdown V2/header/Stuff/Shikigami.hpp
@@ -3,15 +3,42 @@
 
 #include <cstdint>
 
-enum class HelpfulnessType : std::uint8_t {
+class Character;
+class CurseUser;
+
+enum class SupportType : std::uint8_t {
     Offense,
     Boost,
     Protection,
     Regeneration
 };
 
+enum class SummonType : std::uint8_t {
+    Shadow,     // disabled       | not vulnerable
+    Support,    // halved support | |
+    Active      // full support   | vulnerable
+};
+
+struct HealthValues final {
+    double health{1.0};
+    double max_health{1.0};
+    double regen_speed{1.0};
+};
+
+struct ShikigamiValues final {
+    double cost{1.0};
+    double support_value{1.0}; // provides X amount of support/damage/boost
+};
+
+struct ShikigamiType final {
+    SummonType summon_type{SummonType::Shadow};
+    SupportType support_type{SupportType::Offense};
+};
+
 struct Shikigami final {
     EntityInfo id;
-    double helpful_value{1.0};
-    HelpfulnessType help_type{HelpfulnessType::Offense};
+    HealthValues hp;
+    Character* target{nullptr}; // focuses support on the target whether it be its owner or an enemy
+    ShikigamiValues val;
+    ShikigamiType types;
 };
